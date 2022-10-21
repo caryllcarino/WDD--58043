c9a2b03d3f855ea8d3cf7e047cf042b5a137e659
Caryll Franz M. Cariño <83687463+caryllcarino@users.noreply.github.com>
2022-10-21 15:14:19 +0800

    Add files via upload

---
+++ demo4.php
@@ -0,0 +1,29 @@
+<html>
+<head><title>My looping statements</title> </head>
+?php
+
+
+//application 1
+for($x=0;$x<11;$x++)
+{
+
+     if($x==11)
+	 {
+	 break;
+	 }
+	 echo "hello $x <br>";
+	 
+}
+
+// application 2
+			for ($x=3;$x<10;$x++)
+			{
+				if($x==11)
+				{
+				break;
+				}
+				echo "hello; $x <br>";
+			}
+?>
+</body>
+</html>
